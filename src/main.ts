e3023257fb55a5a3787bfb484616cbc092814e1d
Diák <petrik@gmail.com>
2026-02-25 12:19:37 +0100

    vegleges

--- src/main.ts
+++ src/main.ts
@@ -11,24 +11,15 @@ class Bejegyzes {
 }
 
 function megjelenit(bejegyzes: Bejegyzes) {
-    const title = document.getElementById("title") as HTMLElement;
-    const content = document.getElementById("content") as HTMLElement;
-    const color = document.getElementById("color") as HTMLElement;
-   
-
-     if (title?.textContent && content?.textContent === "") {
-        console.error("Hiba: A cím és a tartalom nem lehet üres.");
-     }
-
-     if(color && color.textContent && (color?.textContent === "" || color?.textContent.length > 7 || !color?.textContent.startsWith("#"))) {
-        console.error("Hiba: A szín nem lehet üres vagy nem megfelelő formátumú.");
-     }
+    
 
      const poszt = document.getElementById("posts") as HTMLElement;
-     const posztMain = document.createElement("article") as HTMLElement;
+     const posztMain = document.createElement("main") as HTMLElement;
      posztMain.innerHTML = `
+     <article>
      <h2>${bejegyzes.title}</h2>
      <p>${bejegyzes.content}</p>
+    </article>
      `;
      posztMain.style.backgroundColor = bejegyzes.color;     
       
@@ -40,6 +31,36 @@ function megjelenit(bejegyzes: Bejegyzes) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const bejegyzes = new Bejegyzes("Első bejegyzés", "Ez az első bejegyzés tartalma.", "#ffcc00");
-    megjelenit(bejegyzes);
-});
+
+  
+    const bejegyzes1 = new Bejegyzes("Első bejegyzés", "Ez az első bejegyzés tartalma.", "#ffcc00");
+    megjelenit(bejegyzes1);
+    const bejegyzes2 = new Bejegyzes("Második bejegyzés", "Ez a második bejegyzés tartalma.", "#00ccff");
+    megjelenit(bejegyzes2);
+    const gomb = document.getElementById("add") as HTMLButtonElement;
+    gomb.addEventListener("click", (e) => {
+        e.preventDefault();
+        const titleInput = document.getElementById("title") as HTMLInputElement;
+        const contentInput = document.getElementById("content") as HTMLInputElement;
+        const colorInput = document.getElementById("color") as HTMLInputElement;
+
+        const title = titleInput.value;
+        const content = contentInput.value;
+        const color = colorInput.value;
+
+        if (title === "" || content === "") {
+            console.error("Hiba: A cím és a tartalom nem lehet üres.");
+            return;
+        }
+
+        if (color === "" || color?.length > 7 || !color?.startsWith("#")) {
+            console.error("Hiba: A szín nem lehet üres vagy nem megfelelő formátumú.");
+            return;
+        }
+
+        const newBejegyzes = new Bejegyzes(title, content, color);
+        megjelenit(newBejegyzes);
+    
+    }
+  )}
+  );
